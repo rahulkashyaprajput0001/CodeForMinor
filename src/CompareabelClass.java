d2490fb1e53b4ab0b7d1a0aa8c08bccb9e03b60f
rahulkashyaprajput <kashyap8566@gmail.com>
2022-05-12 17:51:32 +0530

    Commit Compare and ArrayPuzzle

--- src/CompareabelClass.java
+++ src/CompareabelClass.java
@@ -11,8 +11,9 @@ public class CompareabelClass {
         ls.add(new Age(1));
         ls.add(new Age(0));ls.add(new Age(14));
         ls.add(new Age(4));
+        ls.stream().filter(l -> l.age>13).map(l -> l.age).forEach(System.out ::println);
       //  Collections.sort(ls);
-          ls.stream().sorted((o1, o2) -> o1.age- o2.age).forEach(System.out ::println);
+       //   ls.stream().sorted((o1, o2) -> o1.age- o2.age).forEach(System.out ::println);
       //  System.out.println(ls);
     }
 }
